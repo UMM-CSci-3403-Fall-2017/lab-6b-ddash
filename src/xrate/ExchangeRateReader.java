3e146d9054898a033d46005b15b9780384b4240e
debat006 <kyle_debates@yahoo.com>
2017-10-31 19:23:55 -0500

    Finish 'ExchangeRateReader.java'. All tests passing.

--- src/xrate/ExchangeRateReader.java
+++ src/xrate/ExchangeRateReader.java
@@ -1,5 +1,14 @@
 package xrate;
 
+import java.net.*;
+import java.io.*;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 /**
  * Provide access to basic currency exchange rate services.
  * 
@@ -40,8 +49,26 @@ public class ExchangeRateReader {
      * @throws SAXException
      */
     public float getExchangeRate(String currencyCode, int year, int month, int day) {
-        // TODO Your code here
-        throw new UnsupportedOperationException();
+          try{		  
+              String fullURL = baseURL + year + "/" + String.format("%02d", month) + "/" + String.format("%02d", day) + ".xml";
+	     
+	      Document doc = createDocument(fullURL);
+
+               NodeList nodeList = doc.getElementsByTagName("fx");
+
+                for(int i = 0; i < nodeList.getLength(); i++){
+          
+			Element current = (Element) nodeList.item(i);
+          		String currentCurrencyCode = getTagData(current, "currency_code");
+
+          		if(currencyCode.equals(currentCurrencyCode)){
+           	           return Float.parseFloat(getTagData(current, "rate"));
+	  		}
+                }
+           
+	  }catch(Exception ex){}
+       		// Return -1.0 if the exchange rate could not be calculated
+             return -1.0f;
     }
 
     /**
@@ -64,7 +91,66 @@ public class ExchangeRateReader {
     public float getExchangeRate(
             String fromCurrency, String toCurrency,
             int year, int month, int day) {
-        // TODO Your code here
-        throw new UnsupportedOperationException();
+        
+	    float fromCurrencyRate = -1.0f, toCurrencyRate = -1.0f;
+
+	    try{
+                String fullURL = baseURL + year + "/" + String.format("%02d", month) + "/" + String.format("%02d", day) + ".xml";
+
+                Document doc = createDocument(fullURL);
+
+                NodeList nodeList = doc.getElementsByTagName("fx");
+
+
+               for(int i = 0; i < nodeList.getLength(); i++){
+
+                Element current = (Element) nodeList.item(i);
+
+                String currentCurrencyCode = getTagData(current, "currency_code");
+
+                      if(fromCurrency.equals(currentCurrencyCode)){
+                          System.out.println(getTagData(current,"rate"));
+                          fromCurrencyRate = Float.parseFloat(getTagData(current, "rate"));
+                       }
+		      else if(toCurrency.equals(currentCurrencyCode)){
+                          System.out.println(getTagData(current,"rate"));
+                          toCurrencyRate = Float.parseFloat(getTagData(current, "rate"));
+                      }
+		}
+
+
+             System.out.println(fromCurrencyRate + " " + toCurrencyRate);
+                     if(fromCurrencyRate != -1.0 && toCurrencyRate != -1.0){
+                      return fromCurrencyRate / toCurrencyRate;
+		     }
+                } catch(Exception ex){}
+
+           // Return -1.0 if the exchange rate could not be calculated
+           return fromCurrencyRate;
     }
+
+
+       public String getTagData(Element fx, String tagName){
+ 	       NodeList elements = fx.getElementsByTagName(tagName);
+              
+	       Element firstElement = (Element) elements.item(0);
+               NodeList children = firstElement.getChildNodes();
+              
+	       String componentText = children.item(0).getNodeValue();
+
+          return componentText;
+       }//end getTagData
+
+        public Document createDocument(String stringURL) throws MalformedURLException, IOException, ParserConfigurationException, SAXException{
+	       URL url = new URL(stringURL);
+               InputStream inputStream = url.openStream();
+
+               DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+               DocumentBuilder builder = builderFactory.newDocumentBuilder();
+               Document doc = builder.parse(inputStream);
+               doc.getDocumentElement().normalize();
+
+           return doc;
+        }//end createDocument
 }
+
